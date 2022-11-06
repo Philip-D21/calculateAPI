101b53fa3a9ddf259ed7c3a6f370f7ac37a111a1
Philip-D21 <philiplekan88@gmail>
2022-11-06 14:30:55 -0800

    added engine to package.json

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
-//const cors = require('cors')
+
+const cors = require('cors')
 
 
 const app = express();
 const port = process.env.PORT || 3000
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 
 
